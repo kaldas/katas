0f5e5af44eec36c5b1c418113d8b6c566f8eb08b
R o d r i g o <rcaldas@worldremit.com>
2018-04-12 16:16:10 +0100

    Added Reverse words in sentence

---
+++ reverse_words_in_string/reverse_words/ReverseWordsTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace reverse_words
+{
+    [TestFixture]
+    public class ReverseWordsTest
+    {
+        [TestCase("foo", "oof")]
+        [TestCase("foo bar cake", "oof rab ekac")]
+        public void Should_reverse_words_in_string(string sentence, string expected)
+        {
+            var wordProcessor = new WordProcessor();
+            var actual = wordProcessor.Reverse(sentence);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class WordProcessor
+    {
+        public string Reverse(string sentence) // O(n)
+        {
+            var delimiter = ' ';
+            var reversedSetence = string.Empty;
+            var reversedWord = string.Empty;
+
+            for (var i = sentence.Length -1; i > -1;i--)
+            {
+                if (sentence[i] == delimiter)
+                {
+                    reversedSetence = delimiter + reversedWord + reversedSetence;
+                    reversedWord = string.Empty;
+                    continue;
+                }
+
+                reversedWord += sentence[i];
+
+                if (i == 0)
+                {
+                    reversedSetence = reversedWord + reversedSetence;
+                }
+            }
+
+            return reversedSetence;
+        }
+    }
+}
