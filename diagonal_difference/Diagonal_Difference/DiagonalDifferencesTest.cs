f63eafb5f820e1eb4d9900f414d75b86f563d69c
R o d r i g o <rcaldas@worldremit.com>
2018-04-12 16:48:46 +0100

    Added diagonal differences

---
+++ diagonal_difference/Diagonal_Difference/DiagonalDifferencesTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace Diagonal_Difference
+{
+    [TestFixture]
+    public class DiagonalDifferencesTest
+    {
+        [Test]
+        public void Should_get_diagonal_differences()
+        {
+            var matrix = new[,] { { 11, 2, 4 }, { 4, 5, 6 }, { 10, 8, -12 } };
+
+            var calculator = new DiagonalDifferenceCalculator();
+            var actualDifference = calculator.GetDifference(matrix, 3);
+
+            Assert.AreEqual(15, actualDifference);
+        }
+    }
+
+    public class DiagonalDifferenceCalculator
+    {
+        public int GetDifference(int[,] matrix, int size)
+        {
+            var primaryDiagonal = 0;
+            var secondaryDiagonal = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                primaryDiagonal += matrix[i,i];
+                secondaryDiagonal += matrix[i, size - 1 - i];
+            }
+
+            var result = primaryDiagonal - secondaryDiagonal;
+            if (result < 0)
+            {
+                result *= -1;
+            }
+
+            return result;
+        }
+    }
+}
